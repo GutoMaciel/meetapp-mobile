875c9f95825857b72df80c3ce2a40eb2e4745b1d
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-10-28 11:39:35 -0300

    design ajustments

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -40,39 +40,3 @@ export const Separator = styled.View`
   background: rgba(255, 255, 255, 0.2);
   margin: 20px 0 30px;
 `;
-
-// import styled from 'styled-components/native';
-
-// import Input from '~/components/Input';
-// import Button from '~/components/Button';
-
-// export const Container = styled.SafeAreaView`
-//   flex: 1;
-// `;
-
-// export const Separator = styled.View`
-//   height: 1px;
-//   background: rgba(255, 255, 255, 0.2);
-//   margin: 0 0 10px;
-// `;
-
-// export const Form = styled.ScrollView.attrs({
-//   showsVerticalScrollIndicator: false,
-//   contentContainerStyle: { paddingTop: 20, paddingLeft: 30, paddingRight: 30 },
-// })`
-//   align-self: stretch;
-//   margin-top: 10px;
-// `;
-
-// export const FormInput = styled(Input)`
-//   margin-bottom: 10px;
-// `;
-
-// export const SubmitButton = styled(Button)`
-//   margin-top: 5px;
-// `;
-
-// export const LogoutButton = styled(Button)`
-//   margin-top: 10px;
-//   background: #d44059;
-// `;
